cb31983c523517ef69e2ae694b5271ae63c5b8d2
siddiq <msiddiq1400@gmail.com>
2023-02-06 15:11:26 +0500

    class will call super automatically

--- src/components/product-list-components.tsx
+++ src/components/product-list-components.tsx
@@ -10,7 +10,6 @@ interface IPostsProps {
 
 
 export default class Posts extends React.Component<IPostsProps, {}> {
-  constructor(props: any) { super(props) }
   render(){
     let productsarr = []
       for (const [i, p] of this.props.products.entries()) {

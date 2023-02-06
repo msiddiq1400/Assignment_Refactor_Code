0163256fea3a976e896cf02c73f085abce9ed487
siddiq <msiddiq1400@gmail.com>
2023-02-06 14:57:57 +0500

    images should have alt tags

--- src/shop-app.tsx
+++ src/shop-app.tsx
@@ -115,7 +115,7 @@ export class ShopApp extends React.Component<
       <React.Fragment>
         <div className={styles.header}>
           <div className={['container', styles.headerImageWrapper].join(' ')}>
-            <img src={logo} className={styles.headerImage} />
+            <img src={logo} className={styles.headerImage} alt="logo.png"/>
           </div>
         </div>
 
@@ -124,8 +124,8 @@ export class ShopApp extends React.Component<
               className={['container', styles.main].join(' ')}
               style={{margin: '50px inherit', display: 'flex', justifyContent: 'space-evenly'}}
            >
-            <img src={img1} style={{maxHeight: "15em", display: 'block'}} />
-            <img src={img2} style={{maxHeight: "15rem", display: 'block'}} />
+            <img src={img1} style={{maxHeight: "15em", display: 'block'}} alt="img1.png"/>
+            <img src={img2} style={{maxHeight: "15rem", display: 'block'}} alt="img2.png"/>
            </span>
         </>
 

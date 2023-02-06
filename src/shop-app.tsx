e8bb304c86c381713efc24174356a77bcd483920
siddiq <msiddiq1400@gmail.com>
2023-02-06 17:07:35 +0500

    wrap api & db calls with try catch

--- src/shop-app.tsx
+++ src/shop-app.tsx
@@ -18,25 +18,29 @@ export class ShopApp extends React.Component<
     this.favClick = this.favClick.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
 
+    //we would use redux or flux for managing our state instead of declaring a state object inside our app
     this.state = { products: [], isOpen: false, isShowingMessage: false, message: '', numFavorites: 0, prodCount: 0 };
 
     fetch('https://fakestoreapi.com/products').then((response) => {
-      let jsonResponse = response.json();
+      try {
+        let jsonResponse = response.json();
+        jsonResponse.then((rawData) => {
+          let data = [];
 
-      jsonResponse.then((rawData) => {
-        let data = [];
-
-        for (let i = 0; i < rawData.length; i++) {
-          let updatedProd = rawData[i];
-          data.push(updatedProd);
-        }
-        this.setState({
-          products: data,
+          for (let i = 0; i < rawData.length; i++) {
+            let updatedProd = rawData[i];
+            data.push(updatedProd);
+          }
+          this.setState({
+            products: data,
+          });
+          this.setState({
+            prodCount: data.length
+          })
         });
-        this.setState({
-          prodCount: data.length
-        })
-      });
+      } catch (err) {
+        alert("some error occured")
+      }
     });
   }
 
@@ -84,27 +88,31 @@ export class ShopApp extends React.Component<
     })
 
     // **this POST request doesn't actually post anything to any database**
-    fetch('https://fakestoreapi.com/products',{
-            method:"POST",
-            body:JSON.stringify(
-                {
-                    title: payload.title,
-                    price: payload.price,
-                    description: payload.description,
-                }
-            )
-        })
-            .then(res=>res.json())
-            .then(json => {
-               (function (t) {
-                 setTimeout(()=>{
-                    t.setState({
-                       isShowingMessage: false,
-                       message: ''
-                    })
-                 }, 2000)
-              })(this);
-            })
+    try {
+        fetch('https://fakestoreapi.com/products',{
+        method:"POST",
+        body:JSON.stringify(
+          {
+            title: payload.title,
+            price: payload.price,
+            description: payload.description,
+          }
+        )
+      })
+        .then(res=>res.json())
+        .then(json => {
+            (function (t) {
+              setTimeout(()=>{
+                t.setState({
+                  isShowingMessage: false,
+                  message: ''
+              })
+            }, 2000)
+        })(this);
+      })
+    } catch (err) {
+      alert("some error occured")
+    }
   }
 
   render() {

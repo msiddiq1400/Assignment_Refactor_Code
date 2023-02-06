8fd34fc491294902ea7a10563fd6716970016bb0
siddiq <msiddiq1400@gmail.com>
2023-02-06 15:52:02 +0500

    code refactor

--- src/components/add-to-favorite-component.tsx
+++ src/components/add-to-favorite-component.tsx
@@ -25,8 +25,11 @@ export const AddToFavoriteComponent: React.FC<{
                     onClick={() => {
                         onFav(product.title);
                     }}
-                    >
-                    <FaStar /> <span className={actionBarItemLabel}>{!!(!!(product.isFavorite)) ? 'Remove from favorites' : 'Add to favorites'}</span>
+                >
+                    <FaStar /> 
+                    <span className={actionBarItemLabel}>
+                        {!!(!!(product.isFavorite)) ? 'Remove from favorites' : 'Add to favorites'}
+                    </span>
                 </span>
             </span>
         </>
